00610cfc168b3d22c7e572d5ba1af96a090f0d54
Garry Newman <garrynewman@users.noreply.github.com>
2021-02-19 17:04:21 +0000

    Gamemode title

--- code/Game.cs
+++ code/Game.cs
@@ -4,7 +4,7 @@
 /// This is the heart of the gamemode. It's responsible
 /// for creating the player and stuff.
 /// </summary>
-[ClassLibrary( "dm98", Title = "Deathmatch 98" )]
+[ClassLibrary( "dm98", Title = "DM98" )]
 partial class DeathmatchGame : Game
 {
 	public DeathmatchGame()
