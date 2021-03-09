99b04ba81a82d6c459fa09a7c19e6f842819c43a
Garry Newman <garrynewman@users.noreply.github.com>
2021-03-09 16:59:22 +0000

    Panel extension

--- code/ui/Crosshair.cs
+++ code/ui/Crosshair.cs
@@ -24,46 +24,12 @@ public class Crosshair : Panel
 	public override void Tick()
 	{
 		base.Tick();
-		PositionFromLocalPlayer();
+		this.PositionAtCrosshair();
 
 		SetClass( "fire", fireCounter > 0 );
 
 		if ( fireCounter > 0 )
 			fireCounter--;
-	}
-
-	void PositionFromLocalPlayer()
-	{
-		var player = Player.Local;
-		if ( player.IsValid() )
-		{
-			PositionFrom( player.EyePos, player.EyeRot, player );
-		}
-	}
-
-	Vector3 lastPos;
-
-	private void PositionFrom( Vector3 eyePos, Rotation eyeRot, Entity source )
-	{
-		var tr = Trace.Ray( eyePos, eyePos + eyeRot.Forward * 1000 )
-						.Size( 1.0f )
-						.Ignore( source )
-						.UseHitboxes()
-						.Run();
-
-		var screenpos = tr.EndPos.ToScreen();
-
-		if ( lastPos == default )
-			lastPos = screenpos;
-
-		if ( screenpos.z < 0 )
-			return;
-
-		lastPos = Vector3.Lerp( lastPos, screenpos, Time.Delta * 1000.0f * lastPos.Distance( screenpos ) );
-
-		Style.Left = Length.Fraction( lastPos.x );
-		Style.Top = Length.Fraction( lastPos.y );
-		Style.Dirty();
 	}
 
 	public override void OnEvent( string eventName )
